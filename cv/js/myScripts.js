badea90b3bb11213e861e62b80f34a6076006dac
Nguyễn Đình Hiếu <51254371+Hieu-iceTea@users.noreply.github.com>
2020-09-09 11:10:33 +0700

    NEW: thêm link các dự án đã tham gia kèm mô tả vào CV

--- cv/js/myScripts.js
+++ cv/js/myScripts.js
@@ -1,6 +1,6 @@
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
         document.getElementById("navbar").style.top = "0";
@@ -9,3 +9,20 @@ window.onscroll = function() {
     }
     prevScrollpos = currentScrollPos;
 }
+
+/* Enable tooltips everywhere */
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+/* Enable popovers everywhere */
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
+
+/* Enable tooltip & popovers everywhere */
+/* Cái này Hiếu tự nghĩ ra 💜 */
+$(function () {
+    $('[data-toggle="tooltip-popover"]').tooltip()
+    $('[data-toggle="tooltip-popover"]').popover()
+})
